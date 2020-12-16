85f0075483a313c9cb9d97b41864b5b68ea79a69
Lars Behrenberg <l.behrenberg@gmail.com>
2020-12-16 10:59:04 +0900

    add header

--- src/pages/veranstaltungen.jsx
+++ src/pages/veranstaltungen.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 import { Layout } from 'layout'
 import { Neuigkeiten } from 'components'
+import { Header, BackButton } from '../components/NeuigkeitenPage/components'
 
 const VeranstaltungenPage = () => (
   <Layout>
     {/* <SEO title="404: Not found" /> */}
+
+    <Header>
+      <div className="wrapper">
+        <BackButton to="/">&#8592;</BackButton>
+        <h1>Neuigkeiten &amp; Veranstaltungen</h1>
+      </div>
+    </Header>
     <Neuigkeiten />
   </Layout>
 )
