5d9d4cd95425773ed61f12f53def07d788db1be2
Lars Behrenberg <l.behrenberg@gmail.com>
2020-12-18 13:52:41 +0900

    redo mobile menu styling

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -29,18 +29,19 @@ var styles = {
     position: 'fixed',
     height: '100%',
     top: '0',
+    width: '100vw',
   },
   bmMenu: {
-    background: 'rgb(22, 27, 46)',
-    padding: '2.5em 1.5em 0',
+    background: '#fefefe',
+    padding: '0 1.5em 0',
     fontSize: '1.15em',
   },
   bmMorphShape: {
     fill: '#373a47',
   },
   bmItemList: {
-    color: 'white',
-    padding: '1.2em',
+    color: '#000',
+    padding: '1.2em 0 0',
     display: 'flex',
     flexDirection: 'column',
   },
@@ -117,20 +118,52 @@ const StyledMobileMenu = styled(MobileMenu)`
   @media (min-width: 1000px) {
     display: none;
   }
+  text-align: center;
 
   h1 {
-    color: white;
     padding-bottom: 0.5rem !important;
     border-bottom: 1px solid ${props => props.theme.colors.primary};
   }
 
   a {
     text-shadow: none;
-    color: #fff;
     padding: 1rem 0;
     display: inline-block;
     background: none;
-    border-bottom: 1px solid ${props => props.theme.colors.primary};
+    border-bottom: 1px solid #33333330;
+
+    &:last-of-type,
+    &:first-of-type {
+      border-bottom: none;
+    }
+  }
+
+  .mobile-footer-links {
+    /* border: 1px solid black; */
+    display: flex;
+
+    padding: 3.5rem 0 0.9rem !important;
+    border-bottom: 1px solid #33333330;
+
+    a {
+      font-size: 0.85em;
+      padding: 0;
+
+      &:first-of-type {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  .mobile-footer-credits {
+    font-size: 0.85em;
+    padding: 1rem 0 0.2rem !important;
+
+    a {
+      padding: 0;
+      text-decoration: underline;
+      color: grey;
+    }
   }
 `
 
@@ -169,8 +202,24 @@ const Header = () => (
 
       <div>
         <StyledMobileMenu styles={styles} right disableAutoFocus>
-          <h1>t.f.b.s - Telgte</h1>
+          <Logo to="/">
+            <img src={LogoImage} alt="tfbs logo" />
+          </Logo>
           <MenuLinks />
+          <div className="mobile-footer-links">
+            <Link to="/impressum">Impressum</Link>
+            <Link to="/datenschutzerklaerung">Datenschutzerklärung</Link>
+          </div>
+          <p className="mobile-footer-credits">
+            t.f.b.s - Telgte © {new Date().getFullYear()} | Design By @{' '}
+            <a
+              href="https://larsbehrenberg.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Lars Behrenberg
+            </a>
+          </p>
         </StyledMobileMenu>
       </div>
     </Wrapper>
