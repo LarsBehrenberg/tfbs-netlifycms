6a748da68f3a0a6d3edabd1a51178d219c431a6d
Las Behrenberg <l.behrenberg@gmail.com>
2021-02-02 12:14:43 +0900

    modified:   src/layout/theme.js

--- src/layout/theme.js
+++ src/layout/theme.js
@@ -1,10 +1,25 @@
 const theme = {
-  maxWidth: '1100px',
+  maxWidth: '1366px',
+  media: {
+    sm: '480px',
+    md: '768px',
+    lg: '1024px',
+  },
   colors: {
     primary: '#FF9934',
     secondary: '#ffb75e',
     grey: '#979797',
   },
+  shadow: {
+    standard: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+    hover: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);',
+  },
 }
 
 export default theme
+
+// 480px: Mobile devices.
+// 481px — 768px: iPads, Tablets.
+// 769px — 1024px: Small screens, laptops.
+// 1025px — 1200px: Desktops, large screens.
+// 1201px and more — Extra large screens, TV.
